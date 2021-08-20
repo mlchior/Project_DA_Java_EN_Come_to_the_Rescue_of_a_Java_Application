5585c5da6fb70264c0c0254f0338ab8347263c3c
Melchior Crinier <melchior.crinier@pole-emploi.fr>
2021-08-20 21:16:36 +0200

    writer

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,6 +4,7 @@ package com.hemebiotech.analytics;
  */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
@@ -11,44 +12,55 @@ import java.util.Map;
 import java.io.IOException;
 
 public class AnalyticsCounter {
+	Map<String, Integer> mySymptoms;
+	public AnalyticsCounter() {
+	mySymptoms = new HashMap<String, Integer>();
+}
 
-	public static void main(String args[]) throws IOException {
+	public void run() {
 
 		try {
-			BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
 			String line = reader.readLine();
 
-			Map<String, Integer> mySymptoms = new HashMap<String, Integer>();
 
 			while (line != null) {
 				line = reader.readLine();
 
-				if (mySymptoms.containsKey(line)){
-						int add=mySymptoms.get(line);
-						add++;
-						mySymptoms.put(line, add);
-					} else  {
-						mySymptoms.put(line,1);
-					}
-
+				if (mySymptoms.containsKey(line)) {
+					int add = mySymptoms.get(line);
+					add++;
+					mySymptoms.put(line, add);
+				} else {
+					mySymptoms.put(line, 1);
 				}
-				reader.close();
-
-				for (String k : mySymptoms.keySet()){
-					System.out.println(k);
-					System.out.println(mySymptoms.get(k));
-				}
+			}
+			reader.close();
 
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter("Project02Eclipse/src/result.out"));
+			//ouvrir le writer.open
+			//remettre en filewriter
+			// treemap
+			// une function pour lire une function pour trier une function pour ecrire les symptomes
+			// get.symptoms de l'objet read date from file
+			for (String k : mySymptoms.keySet()) {
+				writer.write(k + ":" + mySymptoms.get(k) + "/n");
+				writer.newLine();
+				writer.flush();
+				writer.close();
+			}
+			} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 
+			}
 
-
-	/**	FileWriter writer = new FileWriter("Project02Eclipse/src/result.out");
-
-		writer.close();*/
 	}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,10 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        AnalyticsCounter lunch = new AnalyticsCounter();
+        lunch.run();
+    }
+}
