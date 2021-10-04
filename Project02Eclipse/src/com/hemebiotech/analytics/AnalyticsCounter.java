fbaffaead7c53aa418563334899fdcdf47595369
Melchior Crinier <melchior.crinier@pole-emploi.fr>
2021-10-04 12:04:59 +0200

    First correction

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -12,18 +12,18 @@ public class AnalyticsCounter {
 	private static int pupilCount = 0;
 
 	public static void main(String args[]) throws Exception {
-		// first get input
+
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int i = 0;
+		int headCount = 0;
 		while (line != null) {
-			i++;	// increment i
+			i++;
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headacheCount++;
-				System.out.println("number of headaches: " + headCount);
+				System.out.println("number of headaches: " + headacheCount++);
 			}
 			else if (line.equals("rash")) {
 				rashCount++;
@@ -32,11 +32,11 @@ public class AnalyticsCounter {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();
 		}
 
 
-		// next generate output
+
 		FileWriter writer = new FileWriter("Project02Eclipse/src/result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
