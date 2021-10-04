38f44909f0f234c561bbccb19a560a70b0600b9e
Melchior Crinier <melchior.crinier@pole-emploi.fr>
2021-10-04 14:24:34 +0200

    delete writer method

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,65 +3,61 @@ package com.hemebiotech.analytics;
  * @author melchior crinier
  */
 
-import java.io.BufferedReader;
+
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
+import  java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.io.IOException;
-import java.io.Writer;
 import java.io.File;
-import static java.lang.System.out;
+
 
 public class AnalyticsCounter {
+	private String filepath;
 	Map<String, Integer> mySymptoms;
+
 	public AnalyticsCounter() {
 		mySymptoms = new HashMap<String, Integer>();
 	}
 
-	public void run() {
-
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
-			String line = reader.readLine();
-
-
-			while (line != null) {
-				line = reader.readLine();
-
-				if (mySymptoms.containsKey(line)) {
-					int add = mySymptoms.get(line);
-					add++;
-					mySymptoms.put(line, add);
-				} else {
-					mySymptoms.put(line, 1);
-				}
+	public Map<String, Integer> countSymptoms(List<String> result){
+		Map <String, Integer> symptoms = new HashMap<String, Integer>();
+		for (String Element: result) {
+			if (symptoms.containsKey(Element)) {
+				int add = symptoms.get(Element);
+				add++;
+				symptoms.put(Element, add);
+			} else {
+				symptoms.put(Element, 1);
 			}
-			reader.close();
-
-			for (String clé : mySymptoms.keySet()) {
-				if (clé != null){
-					System.out.println(clé);
-					System.out.println(mySymptoms.get(clé));
-				}}
-
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		return symptoms;
+	}
+
+	public void run() {
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+
+		List<String> result = readSymptomDataFromFile.GetSymptoms();
+
+
+		mySymptoms = countSymptoms(result);
+
+
 
 		try {
 			File fichier = new File("result.out");
+			//writer = new BufferedWriter(new FileWriter(fishier));
 
-
-			FileWriter filewriter = new FileWriter(fichier, true);
+			FileWriter filewriter = new FileWriter(fichier, false);
 			BufferedWriter out = new BufferedWriter(filewriter);
 
-			for (String clé : mySymptoms.keySet())if (clé != null){ {
-				out.write(clé + ":" + mySymptoms.get(clé));
-				out.newLine();
-				out.flush();
-			}}
+			for (String clé : mySymptoms.keySet()){
+				if (clé != null){
+					out.write(clé + ":" + mySymptoms.get(clé));
+					out.newLine();
+					out.flush();
+				}}
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
