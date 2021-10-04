3918150ed6a070e8b2a03f2632542d2ea035d2d7
Melchior Crinier <melchior.crinier@pole-emploi.fr>
2021-10-04 13:44:41 +0200

    add a Map for symptoms list

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,46 +1,49 @@
 package com.hemebiotech.analytics;
+/**
+ * @author melchior crinier
+ */
+
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.List;
 import java.util.HashMap;
+
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+	private static int anxietyCount = 0;
 
 	public static void main(String args[]) throws Exception {
-
+		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount++);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		Map<String, Integer> mySymptoms = new HashMap<String, Integer>();
 
+
+		while (line != null ) {
+
+			if (mySymptoms.containsKey(line)){
+				int add=mySymptoms.get(line);
+				add++;
+				mySymptoms.put(line, add);
+			} else  {
+				mySymptoms.put(line,1);
+			}
 			line = reader.readLine();
+
+		}
+		for (String k : mySymptoms.keySet()){
+			System.out.println(k);
+			System.out.println(mySymptoms.get(k));
 		}
 
 
-
 		FileWriter writer = new FileWriter("Project02Eclipse/src/result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+				writer.close();
 	}
 }
