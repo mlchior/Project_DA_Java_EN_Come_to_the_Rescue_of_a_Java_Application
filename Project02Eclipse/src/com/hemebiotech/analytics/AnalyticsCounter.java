a67a576c1f1e044d40b81dadd646a43c8f00b414
Melchior Crinier <melchior.crinier@pole-emploi.fr>
2021-10-04 14:14:10 +0200

    add Main class

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,47 +3,59 @@ package com.hemebiotech.analytics;
  * @author melchior crinier
  */
 
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
-
 import java.util.Map;
+import java.io.IOException;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	private static int anxietyCount = 0;
-
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
-
-		Map<String, Integer> mySymptoms = new HashMap<String, Integer>();
-
-
-		while (line != null ) {
-
-			if (mySymptoms.containsKey(line)){
-				int add=mySymptoms.get(line);
-				add++;
-				mySymptoms.put(line, add);
-			} else  {
-				mySymptoms.put(line,1);
-			}
-			line = reader.readLine();
-
-		}
-		for (String k : mySymptoms.keySet()){
-			System.out.println(k);
-			System.out.println(mySymptoms.get(k));
-		}
-
-
-		FileWriter writer = new FileWriter("Project02Eclipse/src/result.out");
-				writer.close();
+	Map<String, Integer> mySymptoms;
+	public AnalyticsCounter() {
+		mySymptoms = new HashMap<String, Integer>();
 	}
+
+	public void run() {
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
+			String line = reader.readLine();
+
+
+			while (line != null) {
+				line = reader.readLine();
+
+				if (mySymptoms.containsKey(line)) {
+					int add = mySymptoms.get(line);
+					add++;
+					mySymptoms.put(line, add);
+				} else {
+					mySymptoms.put(line, 1);
+				}
+			}
+			reader.close();
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter("Project02Eclipse/src/result.out"));
+			for (String k : mySymptoms.keySet()) {
+				writer.write(k + ":" + mySymptoms.get(k) + "/n");
+				writer.newLine();
+				writer.flush();
+				writer.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
 }
